6554fd843c80230a35bd26320a11bb6ae53408e9
Error <88781785+TheSadError@users.noreply.github.com>
2022-07-13 15:04:31 +0400

    Add files via upload

---
+++ spydoor.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+#define ll long long
+
+int main(){
+    cout<<"[+] Welcome To SpyDoor!"<<endl;
+    system("python3 spydoor.py");
+    return 0;
+}
